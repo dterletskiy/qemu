a3527c4055c3fd665d086153908273050d88491d
Dmitriy Terletskiy <daterletskiy@gmail.com>
2025-01-10 21:46:55 +0200

    update

--- target/arm/helper.c
+++ target/arm/helper.c
@@ -8451,27 +8451,27 @@ static const ARMCPRegInfo fgt_reginfo[] = {
       .fieldoffset = offsetof(CPUARMState, cp15.fgt_exec[FGTREG_HFGITR]) },
 };
 
-// static void vncr_write(CPUARMState *env, const ARMCPRegInfo *ri,
-//                        uint64_t value)
-// {
-//     /*
-//      * Clear the RES0 bottom 12 bits; this means at runtime we can guarantee
-//      * that VNCR_EL2 + offset is 64-bit aligned. We don't need to do anything
-//      * about the RESS bits at the top -- we choose the "generate an EL2
-//      * translation abort on use" CONSTRAINED UNPREDICTABLE option (i.e. let
-//      * the ptw.c code detect the resulting invalid address).
-//      */
-//     env->cp15.vncr_el2 = value & ~0xfffULL;
-// }
+static void vncr_write(CPUARMState *env, const ARMCPRegInfo *ri,
+                       uint64_t value)
+{
+    /*
+     * Clear the RES0 bottom 12 bits; this means at runtime we can guarantee
+     * that VNCR_EL2 + offset is 64-bit aligned. We don't need to do anything
+     * about the RESS bits at the top -- we choose the "generate an EL2
+     * translation abort on use" CONSTRAINED UNPREDICTABLE option (i.e. let
+     * the ptw.c code detect the resulting invalid address).
+     */
+    env->cp15.vncr_el2 = value & ~0xfffULL;
+}
 
-// static const ARMCPRegInfo nv2_reginfo[] = {
-//     { .name = "VNCR_EL2", .state = ARM_CP_STATE_AA64,
-//       .opc0 = 3, .opc1 = 4, .crn = 2, .crm = 2, .opc2 = 0,
-//       .access = PL2_RW,
-//       .writefn = vncr_write,
-//       .nv2_redirect_offset = 0xb0,
-//       .fieldoffset = offsetof(CPUARMState, cp15.vncr_el2) },
-// };
+static const ARMCPRegInfo nv2_reginfo[] = {
+    { .name = "VNCR_EL2", .state = ARM_CP_STATE_AA64,
+      .opc0 = 3, .opc1 = 4, .crn = 2, .crm = 2, .opc2 = 0,
+      .access = PL2_RW,
+      .writefn = vncr_write,
+      .nv2_redirect_offset = 0xb0,
+      .fieldoffset = offsetof(CPUARMState, cp15.vncr_el2) },
+};
 
 #endif /* TARGET_AARCH64 */
 
@@ -9980,7 +9980,7 @@ void register_cp_regs_for_features(ARMCPU *cpu)
     }
 
     if (cpu_isar_feature(aa64_nv2, cpu)) {
-        // define_arm_cp_regs(cpu, nv2_reginfo);
+        define_arm_cp_regs(cpu, nv2_reginfo);
     }
 
     if (cpu_isar_feature(aa64_nmi, cpu)) {
--- target/arm/kvm.c
+++ target/arm/kvm.c
@@ -948,7 +948,7 @@ bool write_kvmstate_to_list(ARMCPU *cpu)
         uint32_t v32;
         int ret;
 
-        uint32_t regidx_ = kvm_to_cpreg_id(cpu->cpreg_indexes[i]);
+        uint32_t regidx_ = kvm_to_cpreg_id(regidx);regidx
         const ARMCPRegInfo *ri;
         ri = get_arm_cp_reginfo(cpu->cp_regs, regidx_);
 
@@ -1977,18 +1977,18 @@ int kvm_arch_init_vcpu(CPUState *cs)
     }
 
     // @TDA: ToDo
-    cpu->isar.id_aa64mmfr1 &= ~(0xFULL << 8);
-    ret = write_sys_reg64(cs->kvm_fd, &cpu->isar.id_aa64mmfr1,
-                          ARM64_SYS_REG(3, 0, 0, 7, 1));
-    if (ret) {
-        return ret;
-    }
-    cpu->isar.id_aa64mmfr4 &= ~(0xFULL << 24);
-    ret = write_sys_reg64(cs->kvm_fd, &cpu->isar.id_aa64mmfr4,
-                          ARM64_SYS_REG(3, 0, 0, 7, 4));
-    if (ret) {
-        return ret;
-    }
+    // cpu->isar.id_aa64mmfr1 &= ~(0xFULL << 8);
+    // ret = write_sys_reg64(cs->kvm_fd, &cpu->isar.id_aa64mmfr1,
+    //                       ARM64_SYS_REG(3, 0, 0, 7, 1));
+    // if (ret) {
+    //     return ret;
+    // }
+    // cpu->isar.id_aa64mmfr4 &= ~(0xFULL << 24);
+    // ret = write_sys_reg64(cs->kvm_fd, &cpu->isar.id_aa64mmfr4,
+    //                       ARM64_SYS_REG(3, 0, 0, 7, 4));
+    // if (ret) {
+    //     return ret;
+    // }
 
 
     if (cpu_isar_feature(aa64_sve, cpu)) {
