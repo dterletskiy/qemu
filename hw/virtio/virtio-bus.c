70ecf601490de88539201d9eba4ebace511c8721
Dmitriy Terletskiy <daterletskiy@gmail.com>
2025-08-11 13:34:53 +0300

    update
    Signed-off-by: Dmitriy Terletskiy <daterletskiy@gmail.com>

--- hw/virtio/virtio-bus.c
+++ hw/virtio/virtio-bus.c
@@ -81,6 +81,9 @@ void virtio_bus_device_plugged(VirtIODevice *vdev, Error **errp)
     }
 
     vdev->dma_as = &address_space_memory;
+    TDA_PRINTF("vdev->dma_as: %p\n", vdev->dma_as);
+
+    TDA_PRINTF("has_iommu: %s\n", has_iommu ? "TRUE" : "FALSE");
     if (has_iommu) {
         vdev_has_iommu = virtio_host_has_feature(vdev, VIRTIO_F_IOMMU_PLATFORM);
         /*
@@ -89,8 +92,10 @@ void virtio_bus_device_plugged(VirtIODevice *vdev, Error **errp)
          * we fail the device.
          */
         virtio_add_feature(&vdev->host_features, VIRTIO_F_IOMMU_PLATFORM);
+        TDA_PRINTF("klass->get_dma_as: %p\n", klass->get_dma_as);
         if (klass->get_dma_as) {
             vdev->dma_as = klass->get_dma_as(qbus->parent);
+            TDA_PRINTF("vdev->dma_as: %p\n", vdev->dma_as);
             if (!vdev_has_iommu && vdev->dma_as != &address_space_memory) {
                 error_setg(errp,
                        "iommu_platform=true is not supported by the device");
